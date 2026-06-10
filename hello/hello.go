0b33409606affcf0432d51d246d30f87079f60e5
Casper Norspang <casper@norspang.dk>
2026-06-10 22:01:15 +0200

    Update

--- hello/hello.go
+++ hello/hello.go
@@ -10,11 +10,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Casper")
+	names := []string{"Casper", "Malene", "Marty"}
+
+	messages, err := greetings.Hellos(names)
+
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
